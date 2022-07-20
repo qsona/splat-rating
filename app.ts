0d588db044a030c6bc870b6be66c4e97ede9de4b
matatabi3 <matatabi.toraneko@gmail.com>
2022-07-20 16:11:44 +0900

    added function for update user profile.

--- app.ts
+++ app.ts
@@ -2,12 +2,16 @@ import express from 'express'
 import passport from 'passport'
 import { prisma } from './src/prismaClient'
 import { SPLAT_RULES_NAME_MAP } from './src/rules'
+const bodyParser = require('body-parser')
 const DiscordStrategy = require('passport-discord').Strategy
 
 require('dotenv').config()
 
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.set('view engine', 'ejs')
 app.use('/assets', express.static(__dirname + '/assets'))
@@ -45,10 +49,33 @@ app.get('/profile/:id', async (req, res) => {
   if (!user) {
     return res.status(404).send('User Not Found')
   }
-  const ratings = await prisma.rating.findMany({
-    where : { userId : user.id }
-  })
   res.render('profile', { user })
+})
+
+app.post('/profile/:id', async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: req.params.id },
+    include: { Rating: true },
+  })
+  // TODO: check current login user === id user
+  if (!user) {
+    return res.status(404).send('User Not Found')
+  }
+
+  const newName = req.body.name
+  // TODO: do validate & csrf token check
+  const result = await prisma.user.update({
+    where: {
+      id: user.id
+    },
+    data: {
+      name: newName,
+    }
+  })
+  if (result.name !== newName) {
+    return res.status(500).send('User Data update failed...')
+  }
+  res.redirect('/profile/' + user.id)
 })
 
 // TODO: showCount, pageId
