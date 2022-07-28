d1284fd5aa79e1eb9755e18dd0359816187fea08
qsona <mori.jmk@gmail.com>
2022-07-28 20:55:17 +0900

    Do not use link

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -5,7 +5,7 @@ const handler: CommandHandler = {
   commandName: 'sr-ping',
   execute: async (interaction) => {
     const rowDash = new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder().setCustomId('dash').setLabel('Dash!').setStyle(ButtonStyle.Danger))
-    const rowJump = new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder().setCustomId('jump').setLabel('Jump!').setStyle(ButtonStyle.Link))
+    const rowJump = new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder().setCustomId('jump').setLabel('Jump!').setStyle(ButtonStyle.Primary))
     const message = `Pong! User: ${interaction.user.username} Server info: ${interaction.guild?.name} ${interaction.guild?.id}`
     await interaction.reply({ content: message, components: [rowDash, rowJump] })
   },
