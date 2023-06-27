c85b360da85b4a65e45efae64bc1bcaf0fb5841d
qsona <mori.jmk@gmail.com>
2023-06-28 06:25:34 +0900

    Add turf war

--- src/rules.ts
+++ src/rules.ts
@@ -1,10 +1,11 @@
-export type SplatRuleSet = 'SplatZones' | 'TowerControl' | 'Rainmaker' | 'ClamBlitz'
+export type SplatRuleSet = 'SplatZones' | 'TowerControl' | 'Rainmaker' | 'ClamBlitz' | 'TurfWar'
 
 export const SPLAT_RULES_NAME_MAP: { code: SplatRuleSet; name: string }[] = [
   { code: 'SplatZones', name: 'ガチエリア' },
   { code: 'TowerControl', name: 'ガチヤグラ' },
   { code: 'Rainmaker', name: 'ガチホコバトル' },
   { code: 'ClamBlitz', name: 'ガチアサリ' },
+  { code: 'TurfWar', name: 'ナワバリバトル' },
 ]
 
 export const getRuleName = (rule: SplatRuleSet) => {
