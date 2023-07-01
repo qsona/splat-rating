2723e99992cfc3ae57c3259f8073e501633d9a6e
qsona <mori.jmk@gmail.com>
2023-07-01 17:07:35 +0900

    Add more gachipower description

--- src/commands/helpers/modals.ts
+++ src/commands/helpers/modals.ts
@@ -4,7 +4,11 @@ import { SplatRuleSet, getRuleName } from '../../rules'
 export const createRegisterAndJoinModal = (rule: SplatRuleSet) => {
   const ruleName = getRuleName(rule)
   const modal = new ModalBuilder().setCustomId(`modal-register-and-join-${rule}`).setTitle(`${ruleName} レーティング登録`)
-  const gachiPowerInput = new TextInputBuilder().setCustomId('gachipowerInput').setLabel('ガチパワー').setRequired(true).setStyle(TextInputStyle.Short)
+  const gachiPowerInput = new TextInputBuilder()
+    .setCustomId('gachipowerInput')
+    .setLabel('ガチパワー (Xマッチでの平均的なパワー)')
+    .setRequired(true)
+    .setStyle(TextInputStyle.Short)
   const firstActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(gachiPowerInput)
   modal.addComponents(firstActionRow)
   return modal
@@ -13,7 +17,11 @@ export const createRegisterAndJoinModal = (rule: SplatRuleSet) => {
 export const createRegisterModal = (rule: SplatRuleSet) => {
   const ruleName = getRuleName(rule)
   const modal = new ModalBuilder().setCustomId(`modal-register-${rule}`).setTitle(`${ruleName} レーティング登録`)
-  const gachiPowerInput = new TextInputBuilder().setCustomId('gachipowerInput').setLabel('ガチパワー').setRequired(true).setStyle(TextInputStyle.Short)
+  const gachiPowerInput = new TextInputBuilder()
+    .setCustomId('gachipowerInput')
+    .setLabel('ガチパワー (Xマッチでの平均的なパワー)')
+    .setRequired(true)
+    .setStyle(TextInputStyle.Short)
   const firstActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(gachiPowerInput)
   modal.addComponents(firstActionRow)
   return modal
